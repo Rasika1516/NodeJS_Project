77d41e9fae8b3bec0ee78ed869e174a44d7ec927
Rasika1516 <rasikanale1516@gmail.com>
2025-06-04 17:16:28 +0530

    Initial setup

---
+++ db/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required: true,
+    },
+    userAddressLine1:{
+        type:String,
+        required:true,
+    },
+     userAddressLine2:{
+        type:String,
+        required:true,
+    },
+    userPhno:{
+        type:Number,
+        required: true
+    }
+},{timestamps: true});
+
+const User = mongoose.model('User',userSchema);
