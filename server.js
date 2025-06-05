036bde60046d64102791fcf0c76b781495bf2d24
Rasika1516 <rasikanale1516@gmail.com>
2025-06-05 07:51:37 +0530

    Adding database connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import express from 'express';
-
+import connectDB from './db/config';
+import { User } from './db/models/user';
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+connectDB();
+app.post('/user',async(req,res)=>{
+    try {
+        const newUser = new User(req.body);
+        const result = await newUser.save();
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(400).json({message:error.message});
+    }
+});
 
 app.get('/',(req,res)=>{
     res.send("this is the root");
