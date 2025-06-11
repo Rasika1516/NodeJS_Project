9acaed6ca322fdf0fe226a833d4830aa55bc1234
Rasika1516 <rasikanale1516@gmail.com>
2025-06-11 12:45:22 +0530

    adding CURD operation in MVC  structure

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,6 +2,7 @@ import connectDB from "../db/config.js";
 import { User } from "../db/models/user.js";
 connectDB();
 
+//Add user
 export const addUser = async (req, res) => {
   try {
     const newUser = new User(req.body);
@@ -12,3 +13,48 @@ export const addUser = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+//get user list
+export const getUser = async(req,res)=>{
+  try {
+    const user =await  User.find({});
+    res.json(user);
+    console.log("Records fetched successfully");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//get one user list
+export const getOneUser = async(req,res)=>{
+  try {
+    const user =await  User.findById(req.params.id);
+    res.json(user);
+    console.log("Records fetched successfully");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//update the user
+export const updateUser = async(req,res)=>{
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(updatedUser);
+    console.log("Record has been updated successfully");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+//delete the User
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.send({ message: "Record has been deleted" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { addUser } from "../controllers/user.js";
+import { addUser,getUser,updateUser ,deleteUser,getOneUser} from "../controllers/user.js";
 
 const router = express.Router();
 
+//add API
 router.post("/addUser", addUser);
+
+//get API
+router.get("/getUser",getUser);
+
+//get API
+router.get("/getUser/:id",getOneUser);
+
+//update API
+router.put("/updateUser/:id",updateUser);
+
+//delete API
+router.delete("/deleteUser/:id",deleteUser);
 
 export { router };
